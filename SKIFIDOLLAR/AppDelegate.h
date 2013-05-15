c6a61a84ebf61d0fc8222ac2defcb052a2448bce
Viviana Liuni <viviana.liuni@gmail.com>
2013-05-15 22:04:35 +0200

    Initial Commit

---
+++ SKIFIDOLLAR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SKIFIDOLLAR
+//
+//  Created by Stardust on 15/05/13.
+//  Copyright (c) 2013 Viviana Liuni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SKIFIDOLLAR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SKIFIDOLLAR
+//
+//  Created by Stardust on 15/05/13.
+//  Copyright (c) 2013 Viviana Liuni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
